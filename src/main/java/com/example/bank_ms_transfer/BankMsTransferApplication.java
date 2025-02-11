608875608341894e117e4170da28a24f193183ba
innasorokina <soroka.inna.evna@gmail.com>
2025-02-11 21:34:45 +0300

    initial config for ms-transfer

---
+++ src/main/java/com/example/bank_ms_transfer/BankMsTransferApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank_ms_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMsTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMsTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bank_ms_transfer/BankMsTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_ms_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMsTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
